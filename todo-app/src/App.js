c207b256643e0c1f97e326fa2df0fe16c864e6e2
VijeshKunnummal <VijeshKunnummal@users.noreply.github.com>
2024-07-16 17:09:27 +0000

    latest code

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import TodoList from './components/TodoList';
 import './App.css';
+// Bootstrap CSS
+//import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+//import "bootstrap/dist/js/bootstrap.bundle.min";
+import Layout from './components/Layout';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+//<TodoList />
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (      
+        <div className="App">
+            <Router>
+              <Layout>
+              <Route exact path="/" component= {TodoList} />
+              {/* />
+              <Route path = "/mybooks" component = {MyBooks} />
+              <Route path = "/favorites" component = {Favorites} /> */}
+              </Layout>
+            </Router>
+        </div>
+    );
 }
-
 export default App;
